fdcf61562d0dea9c3f931ad9735338cd3dc6bd7d
juddfineberg <jfineberg@mab009-2.local>
2016-02-08 19:46:08 -0600

    turn button on
    having problem with list adding in wrong place

--- List.js
+++ List.js
@@ -2,36 +2,36 @@
 // This section of code is how we prepare our code
 // so this it is prepared to react to user events.
 $(document).ready(function() {
-  $(".btn").on("click", addItem);
-  // $(".link-delete").on("click", deleteItem);
-  $("ol").on("click", ".link-delete", deleteItem);
+  $(".btn").on("click", addPlayer);
+  $(".link-delete").on("click", deletePlayer);
+  $("ol").on("click", ".link-delete", deletePlayer);
 });
 
-// This function prompts the user for a to-do item
-// and then appends it to the existing list of items.
+// This function prompts the user for a to-do player
+// and then appends it to the existing list of players.
 // It also updates the counter at the top of the screen.
-function addItem() {
-  var text = window.prompt("New To-Do Item");
+function addPlayer() {
+  var text = window.prompt("Add Player");
   var delete_link = '<a href="#" class="link-delete">(Delete)</a>'
-  // console.log("Yup!");
+  console.log("Yup!");
   $("ol").append("<li>" + text + " " + delete_link + "</li>");
-  var numItems = $("li").length;
+  var numplayers = $("li").length;
 
-  // Challenge: Differentiate between 1 item vs. more items
-  if (numItems == 1) {
-    $(".total").html(numItems + " item");
+  // Challenge: Differentiate between 1 player vs. more players
+  if (numplayers == 1) {
+    $(".total").html(numPlayers + " player");
   }
   else {
-    $(".total").html(numItems + " items");
+    $(".total").html(numPlayers + " players");
   }
 }
 
-// This function deletes an item from the list.
+// This function deletes an player from the list.
 // The "event" parameter holds information on which
-// item the user wishes to remove.
-function deleteItem(event) {
+// player the user wishes to remove.
+function deleteplayer(event) {
   console.info(event);
-  // $(event.target).remove();
+  $(event.target).remove();
   $(event.target).parent().fadeOut();
 
 }
